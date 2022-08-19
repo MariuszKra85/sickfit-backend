9994316d458382c722974c038c4bd6c1e66b90ef
Mariusz Krawczyk <przyslony@gmail.com>
2022-08-19 11:43:44 +0100

    practice with wes bos project. half way.

---
+++ schemas/Product.ts
@@ -0,0 +1,48 @@
+import { integer, relationship, select, text } from '@keystone-next/fields';
+import { list } from '@keystone-next/keystone/schema';
+
+export const Product = list({
+  // access
+  fields: {
+    name: text({ isRequired: true }),
+    description: text({
+      isRequired: true,
+      ui: {
+        displayMode: 'textarea',
+      },
+    }),
+    status: select({
+      options: [
+        {
+          label: 'draft',
+          value: 'DRAFT',
+        },
+        {
+          label: 'Available',
+          value: 'AVAIABLE',
+        },
+        {
+          label: 'Unavaiable',
+          value: 'UNAVAIABLE',
+        },
+      ],
+      defaultValue: 'DRAFT',
+      ui: {
+        displayMode: 'segmented-control',
+        createView: {
+          fieldMode: 'hidden',
+        },
+      },
+    }),
+    price: integer(),
+    photo: relationship({
+      ref: 'ProductImage.product',
+      ui: {
+        displayMode: 'cards',
+        cardFields: ['image', 'altText'],
+        inlineCreate: { fields: ['image', 'altText'] },
+        inlineEdit: { fields: ['image', 'altText'] },
+      },
+    }),
+  },
+});
